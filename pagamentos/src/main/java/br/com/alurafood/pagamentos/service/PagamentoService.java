8c3e0c5b345bf0330b2c76f45a2c0b0ebd2f5c9c
Mateus Santana <mateus-sgn@hotmail.com>
2024-05-04 01:50:29 -0300

    feat: Inject ModelMappar dependency

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -5,6 +5,7 @@ import br.com.alurafood.pagamentos.model.Pagamento;
 import br.com.alurafood.pagamentos.model.Status;
 import br.com.alurafood.pagamentos.repository.PagamentoRepository;
 import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
